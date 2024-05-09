cd1b62cadd800899b58f91ec8ba2c9c1269d1a4c
FarhanVirenze <farhanvirenze18@gmail.com>
2024-05-09 18:22:32 +0700

    first commit

---
+++ script/main.js
@@ -0,0 +1,18 @@
+const waktu = document.getElementById("waktu")
+
+function tanggal() {
+    const date = new Date()
+    waktu.innerHTML = date;
+}
+
+setInterval(tanggal, 1000);
+
+tanggal();
+
+function sweetalert() {
+    Swal.fire({
+        title: "Good job!",
+        text: "You clicked the button!",
+        icon: "success"
+      });
+}
